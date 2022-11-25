a46d29f45d164d8193f60914158bf343672bf1b9
Fliegen132 <94917094+Fliegen132@users.noreply.github.com>
2022-11-25 14:25:15 +0700

    Update main.c

--- main.c
+++ main.c
@@ -7,41 +7,41 @@ int main(void)
 int m[SIZE]=  {0};
 int sum =0;
   
-  for(int i = 0; i < SIZE; ++i)
-  {
-    scanf("%d", &m[i]);
-  }
+for(int i = 0; i < SIZE; ++i)
+{
+  scanf("%d", &m[i]);
+}
   
-    for(int i = 0; i < SIZE; ++i)
-  {
-    printf("%d", m[i]);
-  }  
-  printf("\n");
+for(int i = 0; i < SIZE; ++i)
+{
+  printf("%d", m[i]);
+}  
+printf("\n");
   
-    for(int i = SIZE-1; i >= 0; --i)
-  {
-    printf("%d", m[i]);
-  }
-    printf("\n");
+for(int i = SIZE-1; i >= 0; --i)
+{
+  printf("%d", m[i]);
+}
+printf("\n");
   
-    for(int i = 0; i < SIZE; ++i)
-  {
-    if (i % 2 == 0) {
-    printf("%i", m[i]);
-    }
+for(int i = 0; i < SIZE; ++i)
+{
+  if (i % 2 == 0) {
+  printf("%i", m[i]);
   }
-  printf("\n");
+}
+printf("\n");
   
-    for(int i = 0; i < SIZE; ++i)
-  {
-    if (i % 2 > 0) {
-    printf("%i", m[i]);
-    }
-  }
-    for(int i = 0; i < SIZE; ++i)
-  {
-    sum += m[i];
-  }
-  printf("\nсумма = %d", sum);
+for(int i = 0; i < SIZE; ++i)
+{
+  if (i % 2 > 0) {
+  printf("%i", m[i]);
+}
+}
+for(int i = 0; i < SIZE; ++i)
+{
+  sum += m[i];
+}
+printf("\nсумма = %d", sum);
 
 }
